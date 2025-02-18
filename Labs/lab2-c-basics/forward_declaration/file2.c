a714ba653752cdec97c616ecc8425959df894842
Aryan Chaturvedi <aryanchaturvedi075@gmail.com>
2025-02-17 21:34:56 -0400

    end my suffering

---
+++ Labs/lab2-c-basics/forward_declaration/file2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void greeting(){
+    printf("Hello from File 2\n");
+}
---
+++ Playground/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    int count = 0;
+    std::cout << "Hello, World!" << count << std::endl;
+    return 0;
+}
