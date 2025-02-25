31bfcdf7f614167ea2f6f1cde45bbac21f7ee86e
Aryan Chaturvedi <aryanchaturvedi075@gmail.com>
2025-02-25 03:52:07 -0400

    Update addition.c and add C++ example for inter-process communication with pipes

--- Playground/cpp-version/ex5.cpp
+++ Playground/cpp-version/ex5.cpp
@@ -1,15 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
+
 int main(){
-    u_int8_t arr[] = {1, 2, 3, 4, 1};           // 0 <= arr[i] <= 255
-    size_t len = sizeof(arr) / sizeof(arr[0]); 
+    u_int8_t arr[] = {1, 2, 3, 4, 1, 2};        // 0 <= arr[i] <= 255
+    size_t len = sizeof(arr) / sizeof(arr[0]);
 
     int fd[2];
-    if(pipe(fd) == -1) exit(EXIT_FAILURE);
+    if (pipe(fd) == -1) exit(EXIT_FAILURE);
 
     pid_t id = fork();
     if (id == -1) {
@@ -21,18 +20,19 @@ int main(){
     size_t start = (id == 0) ? 0 : len / 2;
     size_t end = (id == 0) ? len / 2 : len;
     for(size_t i = start; i < end; i++) sum += arr[i];
-    
-    if(id == 0){
+
+    if (id == 0) {
         close(fd[0]);
         if (write(fd[1], &sum, sizeof(sum)) == -1) return 3;
         close(fd[1]);
-    }
-    else{
-        wait(0);            // waiting for the child process to finish
+    } 
+    
+    else {
+        wait(nullptr);            // waiting for the child process to finish
         close(fd[1]);
         if (read(fd[0], &child_sum, sizeof(child_sum)) == -1) return 4;
         close(fd[0]);
-        printf("The sum of the array is %d\n\n", sum + child_sum);
+        std::cout << "The sum of the array is " << sum + child_sum << "\n\n";
     }
 
     return EXIT_SUCCESS;
