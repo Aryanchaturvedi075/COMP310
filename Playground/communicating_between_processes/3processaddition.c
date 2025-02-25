23a495ddec4cf7374e5684efa60cb36932339d0a
Aryan Chaturvedi <aryanchaturvedi075@gmail.com>
2025-02-25 12:29:11 -0400

    learnt about system process fail checks, function inlining in C, perrors(), passing-by-pointers, exit() fucntion when sub processes end, using uintN_t for C, and u_intN_t for C++, using size_t for loops, and not sharing a pipe between more than 2 processes

--- Playground/communicating_between_processes/3processaddition.c
+++ Playground/communicating_between_processes/3processaddition.c
@@ -6,18 +6,38 @@
 
 int fd1[2], fd2[2];
 
+
+// how function inlining looks in C
 static inline int fork_fail(){
     close(fd1[0]), close(fd1[1]), close(fd2[0]), close(fd2[1]);
+    perror("An error occured while forking\n");
     return 2;
 }
 
+// no passing by reference in C, so we pass by pointers here. In C++ we'd do (int (&fd)[2])
+static inline int write_fail(int (*fd)[2]){
+    close((*fd)[1]);
+    perror("An error occured while writing to the pipe\n");
+    return 3;
+}
+
+static inline int read_fail(int (*fd)[2]){
+    close((*fd)[0]);
+    perror("An error occured while reading from the pipe\n");
+    return 4;
+}
+
+static inline int wait_fail(){
+    perror("An error occured while waiting for a child process");
+    return 5;
+}
 
 int main(){
     // in this version do the addition with 3 processes and 2 pipes
     uint8_t arr[] = {1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4};
     size_t len = sizeof(arr) / sizeof(arr[0]);
     
-    // approach 2, create 2 pipes between processes
+    // approach 2, create 2 pipes between processes -> better avoids race condition, and only the main process forks.
     if(pipe(fd1) == -1 || pipe(fd2)) exit(EXIT_FAILURE);
 
     uint16_t sum = 0, child1, child2;
@@ -31,9 +51,9 @@ int main(){
         close(fd1[0]);
         start = 0, end = len / 3;
         for(size_t i = start; i < end; i++) sum += arr[i];
-        write(fd1[1], &sum, sizeof(sum));
+        if (write(fd1[1], &sum, sizeof(sum)) == -1) return write_fail(&fd1);
         close(fd1[1]);
-        exit(EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);         // exit status for child processes
     }
 
     if (id1 != 0)
@@ -46,19 +66,20 @@ int main(){
             close(fd2[0]);
             start = len / 3, end = (len / 3) * 2;
             for(size_t i = start; i < end; i++) sum += arr[i];
-            write(fd2[1], &sum, sizeof(sum));
+            if (write(fd2[1], &sum, sizeof(sum)) == -1) return write_fail(&fd2);
             close(fd2[1]);
             exit(EXIT_SUCCESS);
         }
         else
         {
             // Wait for both child processes to complete
-            wait(0), wait(0);
+            if (wait(0) == -1) return wait_fail();
+            if (wait(0) == -1) return wait_fail();
             
             // Read results from both pipes
             close(fd1[1]), close(fd2[1]);
-            read(fd1[0], &child1, sizeof(child1));
-            read(fd2[0], &child2, sizeof(child2));
+            if (read(fd1[0], &child1, sizeof(child1)) == -1) return read_fail(&fd1);
+            if (read(fd2[0], &child2, sizeof(child2)) == -1) return read_fail(&fd2);
             close(fd1[0]), close(fd2[0]);
             
             start = (len / 3) * 2, end = len;
--- Playground/cpp-version/ex5.cpp
+++ Playground/cpp-version/ex5.cpp
@@ -4,9 +4,29 @@
 
 int fd[2];
 
+
+// inlined functions for fail checks
 inline int fork_fail(){
     close(fd[0]), close(fd[1]);
+    std::cerr << "An error occured while forking" << std::endl;
     return 2;
+}
+
+inline int write_fail(){
+    close(fd[1]);
+    std::cerr << "An error occured while writing to the pipe" << std::endl;
+    return 3;
+}
+
+inline int read_fail(){
+    close(fd[0]);
+    std::cerr << "An error occured while reading to the pipe" << std::endl;
+    return 4;
+}
+
+inline int wait_fail(){
+    std::cerr << "An error occured while waiting for a child process" << std::endl;
+    return 5;
 }
 
 int main(){
@@ -14,7 +34,7 @@ int main(){
     u_int8_t arr[] = {1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4};
     size_t len = sizeof(arr) / sizeof(arr[0]);
 
-    // approach one, use 1 pipe to communicate between all 2 process
+    // approach one, use 1 pipe to communicate between all 2 process -> can lead to race conditions
     if (pipe(fd) == -1) exit(EXIT_FAILURE);
 
     u_int16_t sum = 0, child, sub_child;
@@ -32,33 +52,34 @@ int main(){
         {
             close(fd[0]);
             start = 0, end = len / 3;
-            for(size_t i = start; i < end; i++) sum += arr[i];
-            write(fd[1], &sum, sizeof(sum));
+            for(size_t i = start; i < end; i++) sum += arr[i];      // a better way to write for loops using size_t
+            if (write(fd[1], &sum, sizeof(sum)) == -1) return write_fail();
             close(fd[1]);
+            exit(EXIT_SUCCESS);
         }
         else
         {
-            if (wait(nullptr) == -1) return 3;
+            if (wait(nullptr) == -1) return wait_fail();
             start = len / 3, end = (len / 3) * 2;
             for(size_t i = start; i < end; i++) sum += arr[i];
-            read(fd[0], &sub_child, sizeof(sub_child));
+            if (read(fd[0], &sub_child, sizeof(sub_child)) == -1) return read_fail();
             close(fd[0]);
             sum += sub_child;
-            write(fd[1], &sum, sizeof(sum));
+            if (write(fd[1], &sum, sizeof(sum)) == -1) return write_fail();
             close(fd[1]);
+            exit(EXIT_SUCCESS);
         }
     }
     else
     {
-        if (wait(nullptr) == -1) return -1;
+        if (wait(nullptr) == -1) return wait_fail();
         close(fd[1]);
         start = (len / 3) * 2, end = len;
         for(size_t i = start; i < end; i++) sum += arr[i];
-        read(fd[0], &child, sizeof(child));
+        if (read(fd[0], &child, sizeof(child)) == -1) return read_fail();
         close(fd[0]);
         std::cout << "The sum of the array is " << sum + child << "\n\n";
     }
-
 
     return EXIT_SUCCESS;
 }
